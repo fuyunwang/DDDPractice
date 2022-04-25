0d8e9879b973910cde0548b50de3ffc94fadc22e
fuyunwang <2375872953@qq.com>
2022-04-25 18:01:51 +0800

    init project

---
+++ skill_ddd_spring/src/main/java/com/fuyunwang/skill_ddd_spring/SkillDddSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fuyunwang.skill_ddd_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillDddSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillDddSpringApplication.class, args);
+    }
+
+}
---
+++ skill_ddd_spring/src/test/java/com/fuyunwang/skill_ddd_spring/SkillDddSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuyunwang.skill_ddd_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillDddSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
